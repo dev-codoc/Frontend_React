bf3f059197934e2fd1b4f299522aa9068247940c
Devanshu Singh <dev2003.rajput@gmail.com>
2025-04-09 01:29:33 +0530

    Learned the useState concepts and also used the spread operator in the eventListener !!

--- reacting/src/App.jsx
+++ reacting/src/App.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./components/Card";
 function App() {
-
+  // const [val, setVal] = useState(12)
+  const [val, setVal] = useState({name: "Devanshu", isBanned: false});
   return (
     <>
       <Card />
+      {/* <div className="px-4">
+        <h1 className="text-3xl">{val}</h1>
+        <button onClick={()=>setVal((prev)=>prev+1)} className="bg-blue-500 text-white px-3 py-2 rounded-md">Change karo</button>
+      </div> */}
+      <div className="px-4">
+        <h1 className="text-3xl">Name: {val.name}</h1>
+        <h1 className="text-3xl">Banned: {val.isBanned.toString()}</h1>
+        <button onClick={()=>setVal({...val, isBanned: !val.isBanned})} className="bg-blue-500 text-white px-3 py-2 rounded-md">Change karo</button>
+      </div>
     </>
   );
 }
